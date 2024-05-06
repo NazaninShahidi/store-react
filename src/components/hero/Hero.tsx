d299f11193dd1cd8acc9690e5e27d62a8739adc4
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-05-06 14:11:41 +0330

    create hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,55 @@
 import React from "react";
 
 function Hero() {
-  return <div className="hero h-[663px]"></div>;
+  return (
+    <div>
+      <div className="hero h-[663px]">
+        <div className="absolute w-[577px] h-[173px] left-[100px] top-[237px] font-bold text-[64px] flex flex-col space-y-4 leading-[64px]">
+          <p>FIND CLOTHES THAT MATCHES YOUR STYLE</p>
+          <span className="text-[#00000099] text-[16px] font-normal leading-[22px]">
+            Browse through our diverse range of meticulously crafted garments,
+            designed to bring out your individuality and cater to your sense of
+            style.
+          </span>
+          <button className="w-[210px] h-[52px] flex justify-center items-center text-[16px] font-normal bg-[#000000]  rounded-[62px] gap-3 text-white">
+            Shop Now
+          </button>
+
+          <div className="flex flex-row items-center p-0 space-x-6 ">
+            <div className="flex flex-col items-start p-0">
+              <p className="text-[40px] font-bold">200+</p>
+              <span className="text-[16px] font-normal text-[#00000099] leading-3 text-nowrap">
+                International Brands
+              </span>
+            </div>
+            <div className="w-[74px] h-0 border border-[#00000099] rotate-90"></div>
+            <div className="flex flex-col items-start p-0">
+              <p className="text-[40px] font-bold">2,000+</p>
+              <span className="text-[16px] font-normal text-[#00000099] leading-3 text-nowrap">
+                High-Quality Products
+              </span>
+            </div>
+            <div className="w-[74px] h-0 border border-[#00000099] rotate-90"></div>
+            <div className="flex flex-col items-start p-0">
+              <p className="text-[40px] font-bold">30,000+</p>
+              <span className="text-[16px] font-normal text-[#00000099] leading-3 text-nowrap">
+                Happy Customers
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#000000] w-full">
+        <div className="flex justify-between mx-auto items-center max-w-screen-xl p-8">
+          <img src={require("../../assests/1.png")} alt="" />
+          <img src={require("../../assests/2.png")} alt="" />
+          <img src={require("../../assests/3.png")} alt="" />
+          <img src={require("../../assests/4.png")} alt="" />
+          <img src={require("../../assests/5.png")} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
