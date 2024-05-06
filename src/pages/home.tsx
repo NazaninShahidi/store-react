fc3657a88f19b6ba125b8b0735323c1e456dd7f7
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-05-06 14:56:31 +0330

    create new-arrival and topselling component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
+import NewArrival from "../components/new-arrival";
+import TopSelling from "../components/top-selling";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <NewArrival />
+      <div className="w-[1240px] border border-[#0000001a] mx-auto"></div>
+      <TopSelling />
     </div>
   );
 }
