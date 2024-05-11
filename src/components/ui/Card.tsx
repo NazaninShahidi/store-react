c167699616a7c0e273466a4f8c113489a847be45
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-05-11 11:40:28 +0330

    dynamic data

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,101 @@
+import { IProduct } from "../../dummyData";
+
+const Card = ({ item }: { item: IProduct }) => {
+  return (
+    <div>
+      <a href="">
+        <img
+          src={require(`../../assests/products/${item.image}`)}
+          alt={item.title}
+        />
+      </a>
+
+      <div className="flex flex-col space-y-2 mt-2">
+        <a href="#" className="font-bold text-xl">
+          {item.title}
+        </a>
+        <div className="flex flex-row items-center p-0 gap-[13px]">
+          {" "}
+          <div className="flex space-x-2">
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+                fill="#F9940A"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+                fill="#F9940A"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+                fill="#F9940A"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+                fill="#F9940A"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+                fill="#F9940A"
+              />
+            </svg>
+          </div>
+          <span className="font-normal text-[14px]"></span>
+          {`${item.star}/5`}
+        </div>
+        <div className="flex flex-row items-center p-0 space-x-3">
+          {item.discount ? (
+            <>
+              {" "}
+              <span className="font-bold text-2xl">${item.discountPrice}</span>
+              <span className="font-bold text-2xl line-through text-[#00000066]">
+                ${item.price}
+              </span>
+              <div className="flex flex-row items-center justify-center py-3 px-2 bg-[#ff33331a] rounded-[62px]">
+                <span className="text-[#FF3333] font-medium text-xs">{`-${item.discountPercent}%`}</span>
+              </div>
+            </>
+          ) : (
+            <span className="font-bold text-2xl">${item.price}</span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/dummyData.ts
@@ -0,0 +1,130 @@
+export interface IProduct {
+  id: string;
+  title: string;
+  price: string;
+  discount: boolean;
+  discountPrice: string;
+  discountPercent: number;
+  star: number;
+  category: string;
+  new: boolean;
+  topSelling: boolean;
+  image: string;
+}
+
+const data: IProduct[] = [
+  {
+    id: "1",
+    title: "T-SHIRT",
+    price: "120",
+    discount: false,
+    discountPrice: "",
+    discountPercent: 0,
+    star: 4.5,
+    category: "casual",
+    new: true,
+    topSelling: false,
+    image: "T-shirt.png",
+  },
+  {
+    id: "2",
+    title: "SLEEVE STRIPED T-SHIRT",
+    price: "160",
+    discount: true,
+    discountPrice: "130",
+    discountPercent: 30,
+    star: 4.5,
+    category: "casual",
+    new: true,
+    topSelling: false,
+    image: "srriped-t-shirt.png",
+  },
+  {
+    id: "3",
+    title: "SKINNY FIT JEANS",
+    price: "260",
+    discount: true,
+    discountPrice: "240",
+    discountPercent: 30,
+    star: 4.5,
+    category: "casual",
+    new: true,
+    topSelling: false,
+    image: "skinny-fit-jeans.png",
+  },
+  {
+    id: "4",
+    title: "CHECKERED SHIRT",
+    price: "180",
+    discount: false,
+    discountPrice: "",
+    discountPercent: 0,
+    star: 4.5,
+    category: "casual",
+    new: true,
+    topSelling: false,
+    image: "checkered-shirt.png",
+  },
+  {
+    id: "5",
+    title: "VERTICAL STRIPED SHIRT",
+    price: "232",
+    discount: true,
+    discountPrice: "212",
+    discountPercent: 20,
+    star: 5,
+    category: "casual",
+    new: false,
+    topSelling: true,
+    image: "vertical-striped-shirt.png",
+  },
+  {
+    id: "6",
+    title: "COURAGE GRAPHIC T-SHIRT",
+    price: "145",
+    discount: false,
+    discountPrice: "",
+    discountPercent: 0,
+    star: 4,
+    category: "casual",
+    new: false,
+    topSelling: true,
+    image: "graphic-t-shirt.png",
+  },
+  {
+    id: "7",
+    title: "LOOSE FIT BERMUDA SHORTS",
+    price: "180",
+    discount: false,
+    discountPrice: "",
+    discountPercent: 0,
+    star: 4,
+    category: "casual",
+    new: false,
+    topSelling: true,
+    image: "shorts.png",
+  },
+  {
+    id: "8",
+    title: "FADED SKINNY JEANS",
+    price: "210",
+    discount: false,
+    discountPrice: "",
+    discountPercent: 0,
+    star: 4,
+    category: "casual",
+    new: false,
+    topSelling: true,
+    image: "skinny-jeans.png",
+  },
+];
+
+export const getNewProduct = () => {
+  const NewProductList = data.filter((product) => product.new);
+  return NewProductList;
+};
+
+export const getTopSellingProduct = () => {
+  const NewProductList = data.filter((product) => product.topSelling);
+  return NewProductList;
+};
