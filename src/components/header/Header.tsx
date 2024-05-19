8527c89ba8464c38dcbb26f88167486edefbc6b5
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-05-19 10:10:51 +0330

    responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,32 @@
+import { useSelector } from "react-redux";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
 import { FaRegUserCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineClear } from "react-icons/md";
+
 function Header() {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   const products = useSelector((state: any) => state.product.value);
 
   return (
     <nav className="w-full h-auto  bg-white">
       <div className="max-w-screen-xl mx-auto h-20 flex items-center justify-between">
-        <Link to="/">
-          <img src="assests/logo.png" alt="logo" />
-        </Link>
-        <ul className="flex flex-row space-x-7 font-normal text-[16px]">
+        <div className="flex space-x-2">
+          {" "}
+          <button
+            className="md:hidden mx-2 text-2xl"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <GiHamburgerMenu />
+          </button>
+          <Link to="/">
+            <img src="assests/logo.png" alt="logo" />
+          </Link>
+        </div>
+
+        <ul className="hidden md:flex md:flex-row md:space-x-7 font-normal text-[16px] ">
           <li>
             <a href="#newArrivals">New Arrivals</a>
           </li>
@@ -25,12 +39,12 @@ function Header() {
         </ul>
         <div>
           <input
-            className="w-[577px] flex items-start gap-[12px] px-[16px] py-[12px] relative bg-[#efefef] rounded-[62px] overflow-hidden focus:outline-none placeholder:p-4"
+            className="hidden md:w-[577px] md:flex items-start gap-[12px] px-[16px] py-[12px] relative bg-[#efefef] rounded-[62px] overflow-hidden focus:outline-none placeholder:p-4"
             placeholder="Search for products..."
           />
 
           <img
-            className=" w-[24px] h-[24px] absolute top-5 m-2"
+            className="hidden block md:w-[24px] md:h-[24px] absolute top-5 m-2"
             alt="Frame"
             src="assests/search.png"
           />
@@ -50,6 +64,34 @@ function Header() {
           </Link>
         </div>
       </div>
+      {showMenu && (
+        <div className="flex flex-col w-full h-[400px] bg-black absolute top-0  p-5">
+          <div className="flex justify-end w-full">
+            {" "}
+            <button
+              className="md:hidden mx-2 text-2xl text-white"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <MdOutlineClear />
+            </button>
+          </div>
+
+          <div className="flex justify-center flex-col items-center w-full p-2">
+            {" "}
+            <ul className="flex flex-col space-y-5 font-normal text-[16px] text-white">
+              <li className="hover:border-b hover:border-b-white p-3 ">
+                <a href="#newArrivals">New Arrivals</a>
+              </li>
+              <li className="hover:border-b hover:border-b-white p-3 ">
+                <a href="#topSelling">Top Selling</a>
+              </li>
+              <li className="hover:border-b hover:border-b-white p-3 ">
+                <a href="#products">Products</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
