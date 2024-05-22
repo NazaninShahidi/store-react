b9e0a0f127b2564d37f54fba2835b101751b82ce
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-05-22 10:15:31 +0330

    add loading

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="flex justify-center items-center w-full h-[100vh] mx-auto">
+      <img src="assests/logo.png" alt="logo" />
+    </div>
+  );
+}
+
+export default Loading;
