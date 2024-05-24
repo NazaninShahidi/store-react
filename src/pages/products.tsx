f1b4db57dd221de1522bd14fcfb877a41d6d364c
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-05-24 21:36:16 +0330

    ui filter

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from "react";
 import { IProduct, getProductByCategory } from "../dummyData";
 import Card from "../components/ui/Card";
 import Breadcrumbs from "../components/ui/Breadcrumbs";
+import { CiFilter } from "react-icons/ci";
 
 function ProductsPage() {
   const [category, setCategory] = useState("");
   const [productList, setProcutList] = useState<IProduct[]>([]);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   const { state: routerState }: any = useLocation();
 
   useEffect(() => {
@@ -23,13 +25,19 @@ function ProductsPage() {
   }, [category]);
 
   return (
-    <div className="max-w-screen-xl mx-auto p-3">
+    <div className="max-w-screen-xl mx-auto p-3 relative">
       <Breadcrumbs />
-      <div className="flex flex-row space-x-7">
-        <Filter />
+      <div className="flex flex-row space-y-7 md:space-x-7 md:space-y-0">
+        <Filter showMenu={showMenu} setShowMenu={setShowMenu} />
         <div className="flex flex-col">
-          <div className="p-2 my-1">
+          <div className="flex justify-between items-center p-2 my-1">
             <p className="font-bold text-[32px] capitalize">{category}</p>
+            <button
+              className=" md:hidden"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <CiFilter className="w-[24px] h-[24px]" />
+            </button>
           </div>
           <div className="grid grid-cols-2  gap-5 md:grid-cols-3">
             {productList.map((product) => (
